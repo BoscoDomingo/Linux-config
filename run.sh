6e9112ba21b47b51a549c18459226fe6e0c7649c
Bosco Domingo <boscodomingob@gmail.com>
2023-09-18 16:51:05 +0200

    Clean up run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,45 @@
+echo "This installer will automatically update your Linux config. All existing config files will be backed up to *.bak"
+
 sudo apt update && sudo apt upgrade && sudo apt install build-essential
 
+if [ -f ~/.bash_profile ]; then
+	mv ~/.bash_profile ~/.bash_profile.bak
+fi
+ln .bash_profile ~/.bash_profile
+
+if [ -f ~/.bashrc ]; then
+	mv ~/.bashrc ~/.bashrc.bak
+fi
+ln .bashrc ~/.bashrc
+
+ln .gitconfig ~/.gitconfig
+ln .nanorc ~/.nanorc
+
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc
 
+# Install oh-my-posh
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
+
 # Original at https://gist.github.com/BoscoDomingo/62f35772e52178b31353a99d2d80ca77
 mkdir ~/shell_themes && cp ./niceDark.omp.json ~/shell_themes/
 
+# Install rtx
 sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 brew install rtx
+
+# Install Node
 rtx install node
-# For Python
+npm i -g @antfu/ni npm-check-updates
+# Install Python
 # CFLAGS="-I$(brew --prefix openssl)/include" LDFLAGS="-L$(brew --prefix openssl)/lib" rtx install python
 
-ln .gitconfig ~/.gitconfig
-ln .nanorc ~/.nanorc
-ln .profile ~/.profile
-
+# Install extra tools
 sudo apt install bat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
@@ -32,9 +51,11 @@ bottom \
 eza\
 bfs \
 direnv \
-git-delta
+git-delta\
+fzf
 
-mkdir ~/.config/lsd/
-ln lsd.config.yaml ~/.config/lsd/config.yaml
+# To install useful key bindings and fuzzy completion for fzf:
+$(brew --prefix)/opt/fzf/install
 
-npm i -g @antfu/ni npm-check-updates
+# mkdir ~/.config/lsd/
+# ln lsd.config.yaml ~/.config/lsd/config.yaml
