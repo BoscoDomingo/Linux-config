e2ffd0e50af9b2adf5c77b1b665377c997484384
Bosco Domingo <boscodomingob@gmail.com>
2024-05-23 10:15:48 +0200

    feat: add custom Firefox config

---
+++ Firefox/default/prefs/autoload_custom_config.js
@@ -0,0 +1,4 @@
+// The following lines must be present
+pref("general.config.filename", "firefox_custom_config.js");
+pref("general.config.obscure_value", 0);
+pref("general.config.sandbox_enabled", false); // disable sandbox to allow privileged JavaScript
---
+++ Firefox/firefox_custom_config.js
@@ -0,0 +1,35 @@
+// IMPORTANT: This first line must always be a comment
+
+// Method 1
+// const searchKeybind = document.getElementById("key_search2");
+// if (searchKeybind) searchKeybind.remove();
+// To use with ctrl + alt
+// searchKeybind.setAttribute("modifiers", "accel,alt");
+
+// Method 2
+
+try {
+	let {classes:Cc, interfaces:Ci, manager:Cm, utils:Cu} = Components;
+	let Services = globalThis.Services || ChromeUtils.import("resource://gre/modules/Services.jsm").Services;
+	function ConfigJS() { Services.obs.addObserver(this, 'chrome-document-global-created', false); }
+	ConfigJS.prototype = {
+	 observe: function (aSubject) { aSubject.addEventListener('DOMContentLoaded', this, {once: true}); },
+	 handleEvent: function (aEvent) {
+		let document = aEvent.originalTarget;
+		let window = document.defaultView;
+		let location = window.location;
+		if (/^(chrome:(?!\/\/(global\/content\/commonDialog|browser\/content\/webext-panels)\.x?html)|about:(?!blank))/i.test(location.href)) {
+		 if (window._gBrowser) {
+			let attr, elm, key, mbo;
+			 let KEYS = ['key_search2'];
+			 let ATTR = ['key','modifiers','command','oncommand'];
+			 for (key in KEYS){
+				elm = window.document.getElementById(KEYS[key]);
+				if (elm) for (attr in ATTR) if (ATTR[attr] in elm.attributes) elm.removeAttribute(ATTR[attr]);
+			 }
+		 }
+		}
+	 }
+	};
+	if (!Services.appinfo.inSafeMode) { new ConfigJS(); }
+ } catch(e) {Cu.reportError(e);}
