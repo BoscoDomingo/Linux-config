99bf64bb7f168862c99cb929478f8ec92edfc5da
Bosco Domingo <boscodomingob@gmail.com>
2025-01-16 18:11:54 +0100

    feat(nvim): update cursor settings

--- .config/nvim/lua/bosco/init.lua
+++ .config/nvim/lua/bosco/init.lua
@@ -12,31 +12,51 @@ vim.opt.cursorline = true
 -- Use system clipboard. Source: https://www.reddit.com/r/neovim/comments/vxdjyb/neovim_in_wsl2_cant_copypaste_tofrom_system/itiyb3p/
 vim.opt.clipboard = "unnamedplus"
 if vim.fn.has("wsl") == 1 then
-    vim.api.nvim_create_autocmd(
-        "TextYankPost",
-        {
-            group = vim.api.nvim_create_augroup("Yank", {clear = true}),
-            callback = function()
-                vim.fn.system("clip.exe", vim.fn.getreg('"'))
-            end
-        }
-    )
+    vim.api.nvim_create_autocmd("TextYankPost", {
+        group = vim.api.nvim_create_augroup("Yank", {
+            clear = true
+        }),
+        callback = function()
+            vim.fn.system("clip.exe", vim.fn.getreg('"'))
+        end
+    })
 end
 
 -- WSL yank support (slower than above, doesn't work with clipboard correctly. Source: https://superuser.com/a/1557751)
 --[[
 if vim.fn.executable("clip.exe") == 1 then
-    vim.api.nvim_create_augroup("WSLYank", { clear = true })
-    vim.api.nvim_create_autocmd("TextYankPost", {
-        group = "WSLYank",
-        callback = function()
-            if vim.v.event.operator == 'y' then
-                vim.fn.system("clip.exe", vim.fn.getreg('0'))
-            end
-        end
-    })
+		vim.api.nvim_create_augroup("WSLYank", { clear = true })
+		vim.api.nvim_create_autocmd("TextYankPost", {
+				group = "WSLYank",
+				callback = function()
+						if vim.v.event.operator == 'y' then
+								vim.fn.system("clip.exe", vim.fn.getreg('0'))
+						end
+				end
+		})
 end
 --]]
 
 -- Indentation settings
 vim.opt.smartindent = true
+
+-- Cursor settings
+vim.opt.guicursor = "a:ver30-Cursor-blinkwait700-blinkon400-blinkoff250"
+vim.opt.cursorline = true -- Highlight current line
+
+-- :au VimLeave * set guicursor= | call chansend(v:stderr, "\x1b[ q") -- reset cursor to normal on exit (vimscript)
+-- vim.api.nvim_create_autocmd("VimLeave", { -- reset cursor to normal on exit (Lua)
+--     pattern = "*",
+--     callback = function()
+--         vim.opt.guicursor = "a:hor1-blinkon1"
+
+--         -- Reset the GUI cursor to default by clearing the 'guicursor' option
+--         -- vim.opt.guicursor = ""
+
+--         -- Send the escape sequence to stderr to reset the cursor appearance
+--         -- vim.fn.chansend(vim.v.stderr, "\x1b[ q") -- reset cursor to normal on exit
+--     end
+-- })
+
+-- Allow cursor to move one character beyond the end of the line in visual mode
+vim.opt.virtualedit:append("onemore")
