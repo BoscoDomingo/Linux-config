7cbcfe771c148980f3c4d41abda5444c7c72b63a
Bosco Domingo <boscodomingob@gmail.com>
2026-06-01 23:34:14 +0300

    feat(neovim): add lazyvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,2 @@
-vim.g.MYVIMRC = "$XDG_CONFIG_HOME/nvim/init.lua" -- required to reload the config with :luafile $MYVIMRC
-require("bosco") -- Imports my personal config
-
-if vim.g.vscode then
-    -- VSCode extension
-    print("Inside the VS Code extension")
-else
-    -- ordinary Neovim
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
