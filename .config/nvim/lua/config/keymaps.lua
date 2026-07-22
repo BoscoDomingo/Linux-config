c79acf9a16c72f44cd2b26c677e71a101a100843
Bosco Domingo <boscodomingob@gmail.com>
2026-07-22 13:23:37 +0200

    feat(nvim): update settings and plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- the following mappings will produce:
+--	d => "delete"
+-- 	leader d => "cut"
+-- Source: https://stackoverflow.com/questions/11993851/how-to-delete-not-cut-in-vim
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without copying to register", noremap = true })
+vim.keymap.set("n", "x", '"_x', { desc = "Cut without copying to register", noremap = true })
+vim.keymap.set("n", "D", '"_D', { desc = "Delete without copying to register", noremap = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", '""d', { desc = "Cut and copy to register", noremap = true })
+vim.keymap.set("n", "<leader>D", '""D', { desc = "Cut and copy to register", noremap = true })
+vim.keymap.set("n", "<leader>dd", '""dd', { desc = "Cut and copy to register", noremap = true })
--- Archive/neovim/nvim-pre-lazyvim/lua/bosco/remap.lua
+++ Archive/neovim/nvim-pre-lazyvim/lua/bosco/remap.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 -- Remap space as leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -8,7 +9,7 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 -- Save with ctrl + s
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 
--- Use system keyboard
+-- Copy/paste with ctrl + c/x/v
 vim.api.nvim_set_keymap('v', '<C-c>', '"+yi', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-x>', '"+c', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-v>', 'c<ESC>"+p', { noremap = true, silent = true })
@@ -34,3 +35,15 @@ vim.keymap.set("i", "<A-down>", "<Esc>:m+<CR>==gi")
 vim.keymap.set("i", "<A-up>", "<Esc>:m-2<CR>==gi")
 vim.keymap.set("v", "<A-down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-up>", ":m '<-2<CR>gv=gv")
+
+-- the following mappings will produce:
+--	d => "delete"
+-- 	leader d => "cut"
+-- Source: https://stackoverflow.com/questions/11993851/how-to-delete-not-cut-in-vim
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without copying to register", noremap = true })
+vim.keymap.set("n", "x", '"_x', { desc = "Cut without copying to register", noremap = true })
+vim.keymap.set("n", "D", '"_D', { desc = "Delete without copying to register", noremap = true })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", '""d', { desc = "Cut and copy to register", noremap = true })
+vim.keymap.set("n", "<leader>D", '""D', { desc = "Cut and copy to register", noremap = true })
+vim.keymap.set("n", "<leader>dd", '""dd', { desc = "Cut and copy to register", noremap = true })
