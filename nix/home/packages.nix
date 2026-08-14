c81c72da160352e67366e8baa8f0435653459f90
Bosco Domingo <boscodomingob@gmail.com>
2026-08-14 14:15:34 +0200

    chore(deps): update nix flakes

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -4,6 +4,21 @@
 # [Nix rationale](../../Documentation/Nix_exploration.md#51-tool-ownership).
 # Package names are nixpkgs attributes. Search: https://search.nixos.org
 let
+  # nixpkgs 26.08.03 omits the z-async submodule required by zsh-autocomplete.
+  zAsyncSource = pkgs.fetchFromGitHub {
+    owner = "marlonrichert";
+    repo = "z-async";
+    rev = "5370537de80670b4a97e49cd253d15067709c0a6";
+    hash = "sha256-tPosFoZSaUShaRpv7ca9BdOMREfmhnzjd/VKHSshhXo=";
+  };
+  zshAutocomplete = pkgs.zsh-autocomplete.overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      # zsh-autocomplete adds this directory to fpath.
+      install -Dm644 ${zAsyncSource}/z-async \
+        "$out/share/zsh-autocomplete/z-async/z-async"
+    '';
+  });
+
   # nixpkgs-unstable currently ships diffnav 0.12.0, which is broken here.
   # Pin to upstream's last known-good derivation; delete this override once
   # nixpkgs ships a fixed release and `pkgs.diffnav` works again.
@@ -53,7 +68,7 @@ in
     zsh-autosuggestions
     zsh-fast-syntax-highlighting
     zsh-completions
-    zsh-autocomplete
+    zshAutocomplete
 
     jujutsu # `jj`
     jjui # jj TUI
